539920890e90b60f6d65c94238d16d5d8b9fe6ba
YOmprakash <omprakashyamavaram@gmail.com>
2024-03-11 10:19:43 +0530

    resume updated

--- src/componnts/About/index.js
+++ src/componnts/About/index.js
@@ -15,7 +15,7 @@ About Me</h3>
  <h4>A Dedicated Developer</h4>
             <p>At NxtWave’s CCBP 4.0 Intensive, I’m currently learning fullstack development with a specialization in 4.0 technologies. In love with the coding challenges and hands-on projects. With Industry’s first ever IRC 4.0, getting ready to take on new challenges in the tech world.</p>
 
-            <a href='https://s3-ap-south-1.amazonaws.com/nkb-backend-ccbp-media-static/ccbp_prod/media/resume_generated/Omprakash_2024-01-27-120703.pdf' target="_blank" rel="noopener noreferrer">
+            <a href='https://s3-ap-south-1.amazonaws.com/nkb-backend-ccbp-media-static/ccbp_prod/media/resume_generated/Omprakash_2024-03-10-132706.pdf' target="_blank" rel="noopener noreferrer">
           <button className="resume-button" type="button">
            View Resume
           </button>
